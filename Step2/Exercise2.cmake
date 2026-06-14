5899a0f00ee6dc2c54ea9ec057534607c7863b03
matus.benko <matus.benko@gmail.com>
2026-06-14 22:25:59 +0200

    step 2 exercise 2

--- Step2/Exercise2.cmake
+++ Step2/Exercise2.cmake
@@ -4,7 +4,13 @@ cmake_minimum_required(VERSION 3.23)
 function(FilterFoo OutVar)
 # TODO3: Search all the variables in the argument list passed to FilterFoo,
 #        and place those containing "Foo" into the list named by "OutVar"
+  foreach(Item IN LISTS ARGN)
+    if(Item MATCHES Foo)
+      list(APPEND ${OutVar} ${Item})
+    endif()
+  endforeach()
 
+  # message("${OutVar}=${${OutVar}}")
   set(${OutVar} ${${OutVar}} PARENT_SCOPE)
 endfunction()
 
